79aa18bfb323d1d7662e099dc86abf376812ae9b
Lily <lmayhenderson@yahoo.com>
2024-07-25 10:04:57 -0700

    Add profile routes.

--- schedulr-backend/app.js
+++ schedulr-backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./src/routes/userRoutes')
+const profileRoutes = require('./src/routes/profileRoutes')
 
 app.use(express.json());
 app.use('/api', userRoutes);
+app.use('/api', profileRoutes);
 
 module.exports = app;
